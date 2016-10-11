f1f3f533f72543dcee0ef01a4a80e6e7a9aee51d
John Dawson <jdnoswad@gmail.com>
2016-10-11 16:40:06 -0700

    more changes. its kinda broken now

--- js/index.js
+++ js/index.js
@@ -2,31 +2,36 @@ var supermarket = new Object();
 
 supermarket.name = 'Red Wing Market';
 supermarket.maxStock = 200;
-supermarket.currentStock = prompt("enter current stock");
-supermarket.checkStock = function() {
-    return "you have " + ((this.maxStock - this.currentStock) / 2) + " goods left in stock.";
-};
-supermarket.checkStockMessage = function() {
-    if (this.checkStock() < 0) {
+supermarket.currentStock = 123;
+supermarket.stockMissing = supermarket.maxStock - supermarket.currentStock;
+supermarket.missingStock = function () {
+    return ("you are missing " + this.stockMissing + "items.");
+}
+
+supermarket.restockMessage = function() {
+    if (this.stockMissing < 0) {
         return "You are overstocked!";
-    } else if (this.checkStock() == 0){
+    } else if (this.stockMissing == 0){
         return "you are fully stocked.";
-    } else if (this.checkStock() < 25) {
+    } else if (this.stockMissing < 50) {
         return "you do not have to restock any time soon.";
-    } else if (this.checkStock() < 50) {
+    } else if (this.stockMissing < 100) {
         return "you have a lot of goods.";
-    } else if (this.checkStock() < 75) {
+    } else if (this.stockMissing < 150) {
         return "consider restocking soon.";
-    } else if (this.checkStock() < 100) {
+    } else if (this.stockMissing < 200) {
         return "the store is nearly empty! Restock soon.";
-    } else if (this.checkStock() == 100) {
+    } else if (this.stockMissing == 200) {
         return "You are out of goods! Restock immediatly!";
     } else {
         return "You are out of goods and you owe customers! Restock immediatly!";
     }
 };
 var elStockQuantity = document.getElementById('stockQuantity');
-elStockQuantity.textContent = supermarket.checkStock();
+elStockQuantity.textContent = supermarket.missingStock;
 
 var elStockLevel = document.getElementById('stockLevel');
-elStockLevel.textContent = supermarket.checkStockMessage();
+elStockLevel.textContent = supermarket.restockMessage();
+
+var elStoreName = document.getElementById('storeName');
+elStoreName.textContent = supermarket.name;
